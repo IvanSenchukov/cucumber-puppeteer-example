8655cdae83d37ae0cd23dcb4d3cc1f48a981dbfc
Ivan Senchukov <iasench@yandex.ru>
2020-09-23 15:50:39 +0300

    русификация тестов

--- features/support/steps.js
+++ features/support/steps.js
@@ -35,3 +35,33 @@ Then(/^I expect the todo item (\d+) to be "(.*)"$/, async function(
 ) {
   return await this.checkTodoIsInList(todoIndex, todo);
 });
+
+
+Given("у меня есть todo-лист", async function(dataTable) {
+  const rows = dataTable.rows();
+  for (let index = 0; index < rows.length; index++) {
+    await this.writeTodo(rows[index][0]);
+  }
+});
+
+When(/^я добавляю элемент todo-листа "(.*)"$/, async function(todo) {
+  return await this.writeTodo(todo);
+});
+
+When(/^я нажимаю на кнопку удалить todo-элемент с номером (\d+)$/, async function(
+    todoIndex
+) {
+  return await this.deleteTodo(todoIndex);
+});
+
+Then(/^я ожидаю, что в todo-листе будет (\d+) элемента?$/, async function(number) {
+  return await this.checkNumberOfTodos(number);
+});
+
+Then(/^я ожидаю, что todo-элемент под номером (\d+) будет "(.*)"$/, async function(
+    todoIndex,
+    todo
+) {
+  return await this.checkTodoIsInList(todoIndex, todo);
+});
+
